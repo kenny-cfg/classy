c77e63048445041c0127fb52e8b02ec7979e2e3f
Kenny Hung (he) <kenny.cfg@outlook.com>
2026-08-13 19:46:08 +0100

    TODO list working

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("JS is working");
+const todoListDiv = document.getElementById("todo-list");
+
+const todoList = [
+  "Teach students some OOP",
+  "Tell my child off"
+]
+
+
+todoListDiv.textContent = "POO";
