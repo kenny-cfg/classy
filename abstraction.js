cc1ec1254434b2b6c310d36e8eb66f1a421f4f0d
Kenny Hung (he) <kenny.cfg@outlook.com>
2026-08-13 19:30:37 +0100

    Abstract class example

---
+++ abstraction.js
@@ -0,0 +1,13 @@
+/*
+GROUP ACTIVITY: ABSTRACTION
+In groups of 3-4
+1. Create an abstract class Vehicle, a constructor that initialises make,
+model, and year. Add an abstract method
+display_vehicle_info() that should throw an error if it is not
+overridden.
+2. Modify the Car and ElectricCar classes to extend Vehicle and
+implement the display_vehicle_info method to return car
+information for each type, e.g “This is an electric/petrol car”.
+3. Override the estimate_range method to customise behavior for
+electric cars.
+*/
